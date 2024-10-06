81527b71140e02ec74d2b02faa5e875d2e375aa7
caesaryang06 <caesaryang06@163.com>
2024-10-06 23:18:10 +0800

    auto - commit

--- script/addrepository .bat
+++ script/addrepository .bat
@@ -2,18 +2,9 @@
 :: 仓库地址格式为：https://gitee.com/xxx/xxx.git
 :: 仓库地址格式为：https://github.com/xxx/xxx.git
 @echo off
+:: 初始化当前目录为git仓库
+git init
 :: 添加github远程仓库到本地
 git remote add github https://github.com/caesaryang06/pydemo.git
 :: 添加gitee远程仓库到本地
 git remote add gitee https://gitee.com/caesaryang06/pydemo.git
-:: 添加远程仓库到本地
-git remote add origin https://gitee.com/caesaryang06/pydemo.git
-:: 添加远程仓库到本地
-git remote add origin https://github.com/caesaryang06/pydemo.git
-:: 查看远程仓库
-git remote -v
-:: 删除远程仓库
-git remote remove origin
-:: 删除远程仓库
-git remote remove github
-:: 删除远程仓库
